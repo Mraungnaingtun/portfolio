260202dd50e43d73724395994b069ea641500b0f
Purwatsate <ant.pwt@gmail.com>
2024-09-19 17:08:47 +0630

    update and fix

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -34,39 +34,46 @@ export class ProjectComponent {
       development: 'Full Stack',
       description: 'Building a CRUD application based on the MVC framework, leveraging Thymeleaf for user interface management.',
       link: 'https://github.com/Mraungnaingtun/spring-crud-thymeleaf',
-      languages: ['Spring Boot', 'Mysql', 'Thymeleaf']
+      languages: ['Spring MVC', 'Mysql', 'Thymeleaf']
     },
     {
       title: 'Myanmar Gold Calculator',
       development: 'Frontend',
       description: 'Calculator of gold with Myanmar Measurement (Kyat,Pay,Roy) currently used by many users with a mobile app',
       link: 'https://github.com/Mraungnaingtun/shwe',
-      languages: ['React']
+      languages: ['React','Material UI']
     },
     {
       title: 'Oauth2 Resource Server',
       development: 'Backend',
       description: 'Authentication and Authorization with latest spring version and spring security version',
       link: 'https://github.com/Mraungnaingtun/oauth2-resource-server',
-      languages: ['Spring Boot', 'Postgres']
+      languages: ['Spring Boot','JWT' ,'Postgres']
     },
     {
       title: 'Angular & api calling',
       development: 'Frontend',
       description: 'Project with api calling using angular with simple and better way with DTO',
       link: 'https://github.com/Mraungnaingtun/api-call-in-angular',
-      languages: ['Spring Boot', 'React']
+      languages: ['Spring Boot', 'Angular']
     },
     {
       title: 'Portfolio Website',
       development: 'Frontend',
       description: 'A quick introduction to a personal portfolio website showcasing projects and skills.',
       link: 'https://github.com/Mraungnaingtun/portfolio',
-      languages: ['Angular']
+      languages: ['Angular','Firebase']
+    },
+    {
+      title: 'Docker @ Backend',
+      development: 'Backend',
+      description: 'Basic Setup Docker to host in Render Website.',
+      link: 'https://github.com/Mraungnaingtun/docker-testing.git',
+      languages: ['Spring Boot','Docker']
     },
   ];
 
-
+  
   
   getCapital(title: string) {
     return title.toUpperCase();
